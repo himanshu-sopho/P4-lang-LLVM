b25bd9905a343d9df4b37a93880e415df2913f99
SanketKhobragade <cs15bt3ch11020@iith.ac.in>
2018-07-10 16:03:27 +0530

    Without build directory

--- build/IncludeFiles/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sakhobra/Documents/P4-lang-LLVM/IncludeFiles
-# Build directory: /Users/sakhobra/Documents/P4-lang-LLVM/build/IncludeFiles
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
